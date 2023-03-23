f21b200c9554bbc9af3dcc64d74f8a5a0756852b
Amandeep <amandeepnirania@gmail.com>
2023-03-23 22:50:33 +0530

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Footer = () => {
+    const style = {
+        color : "White",
+        marginLeft : "660px",
+        marginTop : "20px"
+      }
+  return (
+    <div>
+     <h4 style={style}>Made with ❤️ by Amandeep</h4> 
+    </div>
+  )
+}
+
+export default Footer
